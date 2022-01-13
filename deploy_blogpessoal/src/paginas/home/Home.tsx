45a6d41631748406b85471d2d6cc3a49cc795733
Vitor Crivelaro <v_crivelaro@hotmail.com>
2022-01-13 18:58:04 -0300

    Adicionando projeto em ReactJS

---
+++ deploy_blogpessoal/src/paginas/home/Home.tsx
@@ -0,0 +1,14 @@
+import React from 'react' ;
+import './Home.css';
+
+function Home(){
+    return(
+        <>
+            <h1 className="titulo">Home</h1>
+            <img src="https://pbs.twimg.com/media/DurHZc-WwAAIsyp?format=jpg&name=medium" 
+            alt="imagem tela inicial " className="img"  />
+        </>
+    );
+}
+
+export default Home;
