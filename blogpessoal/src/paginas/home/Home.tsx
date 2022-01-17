c3f04f18735dfecff2a06c88e855172c023a89e1
Vitor Crivelaro <v_crivelaro@hotmail.com>
2022-01-17 15:41:36 -0300

    adicionando Menu e Rodapé

--- blogpessoal/src/paginas/home/Home.tsx
+++ blogpessoal/src/paginas/home/Home.tsx
@@ -1,12 +1,28 @@
 import React from 'react' ;
+import {Typography, Box, Grid, Button} from '@material-ui/core';
 import './Home.css';
 
 function Home(){
     return(
         <>
-            <h1 className="titulo">Home</h1>
-            <img src="https://pbs.twimg.com/media/DurHZc-WwAAIsyp?format=jpg&name=medium" 
-            alt="imagem tela inicial " className="img"  />
+            <Grid container direction="row" justifyContent="center" alignItems="center" style={{ backgroundColor: "purple" }}>
+                <Grid alignItems="center" item xs={6}>
+                    <Box paddingX={20} >
+                        <Typography variant="h3" gutterBottom color="textPrimary" component="h3" align="center" style={{ color: "white", fontWeight: "bold" }}>Sejam bem vindos(as) ao meu blog pessoal!</Typography>
+                        <Typography variant="h5" gutterBottom color="textPrimary" component="h5" align="center" style={{ color: "white", fontWeight: "bold" }}>Sintam-se livres para expressarem suas opniões sobre nosso conteúdo!</Typography>
+                    </Box>
+                    <Box display="flex" justifyContent="center">
+                        <Box marginRight={1}>
+                        </Box>
+                        <Button variant="outlined" style={{ borderColor: "white", backgroundColor: "#3F51B5", color: "white" }}>Ver Postagens</Button>
+                    </Box>
+                </Grid>
+                <Grid item xs={6} >
+                    <img src="https://pbs.twimg.com/media/DurHZc-WwAAIsyp?format=jpg&name=medium" alt="" width="500px" height="500px" />
+                </Grid>
+                <Grid xs={12} style={{ backgroundColor: "white" }}>
+                </Grid>
+            </Grid>
         </>
     );
 }
