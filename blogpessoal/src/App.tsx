09b5db9c42e90f5b3b8e2a1ae7ea044ded71dcfd
Vitor Crivelaro <v_crivelaro@hotmail.com>
2022-01-20 14:19:50 -0300

    Realizando algumas correções e criado a model e service

--- blogpessoal/src/App.tsx
+++ blogpessoal/src/App.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Navbar from './components/statics/navbar/Navbar';
-import Footer from './components/statics/footer/Footer';
+import Navbar from './components/estaticos/navbar/Navbar';
+import Footer from './components/estaticos/footer/Footer';
+import CadastroUsuario from './paginas/cadastroUsuario/CadastroUsuario';
 import Home from './paginas/home/Home';
 import Login  from './paginas/login/Login';
 import './App.css';
@@ -9,21 +10,29 @@ import './App.css';
 function App() {
   return (
     <Router>
-      <Navbar/>
+      <Navbar />
         <Switch>
           <div style={{minHeight: '100vh'}}>
+
           <Route exact path='/'>
-              <Login/>
+              <Login />
             </Route>
+
           <Route path='/login'>
-              <Login/>
+              <Login />
             </Route>
+
             <Route path='/home'>
-              <Home/>
+              <Home />
             </Route>
+
+            <Route path='/cadastrousuario'>
+              <CadastroUsuario />
+            </Route>
+
           </div>
         </Switch>
-        <Footer/>
+        <Footer />
     </Router>
   )
 }
